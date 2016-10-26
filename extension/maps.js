289c2b1c62a169a5d463242d68dcdae62db38912
Jacques Marcotte <jpmarcotte@gmail.com>
2016-10-26 15:44:13 -0500

    Move storage utility functions to a separate file.

--- extension/maps.js
+++ extension/maps.js
@@ -4,14 +4,6 @@ $('#MainContent_divControlContainer > div.divInline').first().append(
 		'<DIV class="RadComboBox RadComboBox_Black" style="font-size:Larger;"><SPAN class="rcbInner rcbReadOnly"><SPAN class="rcbInput radPreventDecorate" onclick="store_map_data()" style="cursor:pointer">Store Map Data</SPAN></SPAN></DIV>'
 );
 
-document.addEventListener('Clear_Storage', function(e) {
-	chrome.storage.sync.clear();
-});
-
-document.addEventListener('Get_All_Storage', function(e) {
-	chrome.storage.sync.get(function(items){ console.log(items); });
-});
-
 document.addEventListener('Get_Map_Data', function(e) {
 	chrome.storage.sync.get(
 		'map:'+e.detail.map,
--- extension/profile.js
+++ extension/profile.js
@@ -4,14 +4,6 @@ $('div#MainContent_divDropDowns > div.RadDropDownList').last().after(
 		'<DIV class="RadDropDownList RadDropDownList_Black" style="font-size:Larger; cursor:pointer"><SPAN class="rddlInner"><SPAN class="rddlFakeInput" onclick="store_player_data()">Store Player Data</SPAN></SPAN></DIV>'
 );
 
-document.addEventListener('Clear_Storage', function(e) {
-	chrome.storage.sync.clear();
-});
-
-document.addEventListener('Get_All_Storage', function(e) {
-	chrome.storage.sync.get(function(items){ console.log(items); });
-});
-
 document.addEventListener('Get_Player_Data', function(e) {
 	chrome.storage.sync.get(
 		'player:'+e.detail.ID,
