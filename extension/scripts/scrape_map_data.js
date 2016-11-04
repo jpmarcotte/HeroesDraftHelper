a733ecc4f3c315bdd0e9df190a8c1257bec62331
shoemakn <shoebucket@gmail.com>
2016-11-04 13:14:39 -0400

    Map Data feedback

--- extension/scripts/scrape_map_data.js
+++ extension/scripts/scrape_map_data.js
@@ -9,12 +9,15 @@ window.maps_opened = false;
 process_map_rows = function() {
 	if (window.map_rows.length) {
 		console.log("Loading Map, "+window.map_rows.length+" remaining ");
+		// Provide feedback
+		$('#HDH_StoreMapDataButton .lbl').text("Loading Map, "+window.map_rows.length+" maps remaining...");
 		row = window.map_rows.shift();
 		$(row).find('button').click();
 		wait_for_state(page_ready, process_map_rows, 100);
 	} else { window.maps_opened = true; }
 }
 
+window.maps_parsed = false;
 parse_maps = function() {
 	map_keys = {};
 	$("table#ctl00_MainContent_RadGridMapStatistics_ctl00 > thead > tr > th").each(function(i) {
@@ -25,13 +28,18 @@ parse_maps = function() {
 	map_rows = $('table#ctl00_MainContent_RadGridMapStatistics_ctl00 > tbody').children().toArray();
 
 	map_data = {};
+	mapCount = 0;
+	totalMapCount = (map_rows.length / 2);	// There are two rows displayed in the table for each map
 	while (map_rows.length) {
 		map_name_row = map_rows.shift();
 		map_hero_row = map_rows.shift();
 
 		map_name = $(map_name_row).find('td').eq(map_keys['Map Name']).text().trim();
-		console.log("Collecting Data for "+map_name);
-
+		mapCount += 1;
+		console.log("Collecting Data for "+map_name+" ("+mapCount+" / "+totalMapCount+")");
+		// Provide feedback
+		$('#HDH_StoreMapDataButton .lbl').text("Processing map "+mapCount+" of "+totalMapCount+"...");
+		
 		map_hero_table = $(map_hero_row).find('table');
 		hero_keys = {};
 		$(map_hero_table).find('th').each(function(i) {
@@ -60,10 +68,15 @@ parse_maps = function() {
 			'timestamp': Date.now()
 		}}));
 	}
-
+	window.maps_parsed = true;
 }
 
 store_map_data = function() {
+	// Provide feedback
+	$('#HDH_MapProcessingIcon').removeClass().addClass('fa fa-spinner fa-spin');
+	$('#HDH_StoreMapDataButton .lbl').text("Processing...");
+	$('#HDH_StoreMapDataButton').removeClass().addClass('btn btn-info');
+	
 	console.log("Selecting Hero League");
 	$("div#ctl00_MainContent_DropDownGameMode_DropDown li:contains('Hero League')").click();
 	window.maps_ready = false;
@@ -78,4 +91,14 @@ store_map_data = function() {
 	});
 
 	wait_for_state(function(){return window.maps_opened;}, parse_maps, 1000);
+	
+	// Provide feedback
+	wait_for_state(
+		function(){return window.maps_parsed;},
+		function() {
+			$('#HDH_MapProcessingIcon').removeClass().addClass('fa fa-check');
+			$('#HDH_StoreMapDataButton .lbl').text("Map Data Stored!");
+			$('#HDH_StoreMapDataButton').removeClass().addClass('btn btn-success');
+		},
+		1000);
 }
