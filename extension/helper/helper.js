cd9f4abc82c0aa2f185d24ed1eb5f2e7e39bf9cc
Jacques Marcotte <jpmarcotte@gmail.com>
2017-04-27 17:26:00 -0500

    Use Laplace Rule of Succession

--- extension/helper/helper.js
+++ extension/helper/helper.js
@@ -121,7 +121,7 @@ function get_player_suggestions(player_id) {
 		m = map_data[current_map][hero]
 		p = player_data[player_id][hero]
 		if (m && p && p['Win Percent']) {
-			player_confidence = ( p['Win Percent'] * p['Games Played'] - 1 ) / (p['Games Played'] + 1);
+			player_confidence = ( p['Win Percent'] * p['Games Played'] + 1 ) / (p['Games Played'] + 2);
 			score = Math.pow( m['Win Percent'] * player_confidence, 1/2 ) * 10000;
 			possible_heroes.push({'hero':hero, 'score':score});
 		}
