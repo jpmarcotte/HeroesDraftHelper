a3141768e34fbfb73ecd059092668d1f8d43eda6
Jacques Marcotte <jacques.marcotte@neighborhoods.com>
2017-11-06 19:06:46 -0600

    Declare all variables

--- extension/helper/helper.js
+++ extension/helper/helper.js
@@ -13,20 +13,20 @@ $(document).ready(function() {
 	chrome.storage.sync.get(function(items) {
 		console.log(items);
 		for (key in items) {
-			key_type = key.split(':')[0];
+			let key_type = key.split(':')[0];
 			//console.log(key+" is a "+key_type);
 			if (key_type == 'map') {
-				map_name = items[key].map;
+				let map_name = items[key].map;
 				map_data[map_name] = items[key].heroes;
 				$('select#map').append("<OPTION>"+map_name+"</OPTION>");
 			} else if (key_type == 'player') {
-				player = items[key];
+				let player = items[key];
 				player_data[player.ID] = player.heroes;
 				$('select.player_select').append("<OPTION value='"+player.ID+"'>"+player.name+"</OPTION>");
 			} else if (key_type == 'hero_sub_roles') {
 				hero_sub_roles = items[key];
-				for (hero in hero_sub_roles) {
-					role = hero_sub_roles[hero];
+				for (let hero in hero_sub_roles) {
+					let role = hero_sub_roles[hero];
 					sub_role_classes[role] = role.replace(' ','');
 				}
 
@@ -70,23 +70,24 @@ function hero_display(name, score) {
 }
 
 function update_ban_suggestions() {
-	ban_suggestions = get_ban_suggestions();
-	display = "";
+	let ban_suggestions = get_ban_suggestions();
+	let display = "";
 	for (let i = 0; i < num_ban_suggestions; i++) {
-		hero = ban_suggestions[i];
-		name = hero['hero'];
-		score = hero['score'];
+		let hero = ban_suggestions[i];
+		let name = hero['hero'];
+		let score = hero['score'];
 		display += hero_display(name,score.toFixed(0));
 	}
 	$('#suggested_bans').html(display);
 }
 
 function get_ban_suggestions() {
-	possible_bans = [];
+	let possible_bans = [];
 	for (let i=0, len=window.available_heroes.length; i < len; i++) {
-		hero = window.available_heroes[i];
-		if (m = map_data[current_map][hero]) {
-			score = ( m['Games Banned'] + m['Games Played'] ) * m['Win Percent'] ;
+		let hero = window.available_heroes[i];
+		let m = map_data[current_map][hero];
+		if (m) {
+			let score = ( m['Games Banned'] + m['Games Played'] ) * m['Win Percent'] ;
 			possible_bans.push({'hero':hero, 'score':score});
 		}
 	}
@@ -99,13 +100,14 @@ function get_ban_suggestions() {
 
 function update_players_suggestions() {
 	$('select.player_select').each(function(){
-		if (player_id = $(this).val()) {
-			player_suggestions = get_player_suggestions(player_id);
-			display = "";
+	    let player_id = $(this).val();
+		if (player_id) {
+			let player_suggestions = get_player_suggestions(player_id);
+			let display = "";
 			for (let i = 0; i < num_player_suggestions; i++) {
-				hero = player_suggestions[i];
-				name = hero['hero'];
-				score = hero['score'];
+				let hero = player_suggestions[i];
+				let name = hero['hero'];
+				let score = hero['score'];
 				display += hero_display(name,score.toFixed(0));
 			}
 			$(this).closest('tr').find('.hero_suggestions').html(display);
@@ -114,14 +116,14 @@ function update_players_suggestions() {
 }
 
 function get_player_suggestions(player_id) {
-	possible_heroes = [];
+	let possible_heroes = [];
 	for (let i=0, len=window.available_heroes.length; i < len; i++) {
-		hero = window.available_heroes[i]
-		m = map_data[current_map][hero]
-		p = player_data[player_id][hero]
+		let hero = window.available_heroes[i]
+		let m = map_data[current_map][hero]
+		let p = player_data[player_id][hero]
 		if (m && p && p['Win Percent']) {
-			player_confidence = ( p['Win Percent'] * p['Games Played'] + 1 ) / (p['Games Played'] + 2);
-			score = Math.pow( m['Win Percent'] * player_confidence, 1/2 ) * 10000;
+			let player_confidence = ( p['Win Percent'] * p['Games Played'] + 1 ) / (p['Games Played'] + 2);
+			let score = Math.pow( m['Win Percent'] * player_confidence, 1/2 ) * 10000;
 			possible_heroes.push({'hero':hero, 'score':score});
 		}
 	}
@@ -133,23 +135,24 @@ function get_player_suggestions(player_id) {
 }
 
 function update_general_suggestions() {
-	general_suggestions = get_general_suggestions();
-	display = "";
+	let general_suggestions = get_general_suggestions();
+	let display = "";
 	for (let i = 0; i < num_general_suggestions; i++) {
-		hero = general_suggestions[i];
-		name = hero['hero'];
-		score = hero['score'];
+		let hero = general_suggestions[i];
+		let name = hero['hero'];
+		let score = hero['score'];
 		display += hero_display(name,score.toFixed(0));
 	}
 	$('#general_suggestions').html(display);
 }
 
 function get_general_suggestions() {
-	possible_heroes = [];
+	let possible_heroes = [];
 	for (let i=0, len=window.available_heroes.length; i < len; i++) {
-		hero = window.available_heroes[i];
-		if (m = map_data[current_map][hero]) {
-			score = m['Win Percent'] * 1000;
+		let hero = window.available_heroes[i];
+		let m = map_data[current_map][hero];
+		if (m) {
+			let score = m['Win Percent'] * 1000;
 			possible_heroes.push({'hero':hero, 'score':score});
 		}
 	}
@@ -162,13 +165,14 @@ function get_general_suggestions() {
 
 
 function get_available_heroes() {
-	heroes = {};
+	let heroes = {};
 	for (let i = 0, len = hero_names.length; i < len; i++) {
 		heroes[hero_names[i]] = null;
 	}
 
 	$('select.hero_select').each(function(){
-		if ( hero_name = $(this).val() ) {
+	    let hero_name = $(this).val();
+		if (hero_name) {
 			delete heroes[hero_name];
 		}
 	});
